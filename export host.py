71d5860815a4d86e02a7bb4637982c0a498b4973
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2023-07-11 15:31:33 +0300

    Update export host.py

--- export host.py
+++ export host.py
@@ -5,7 +5,6 @@ now = datetime.datetime.now().strftime("%Y%m%d_%H-%M-%S")
 link = "***" 
 name = now + '.txt'
 
-
 z = ZabbixAPI('http://***', user='***', password='***')
 answer = z.do_request('apiinfo.version')
 print ("Zabbix version:",answer['result'])
@@ -14,7 +13,6 @@ print('Start')
 groups = z.hostgroup.get(output=['groupid','name'])
 for group in groups:
   print ("Export group:", group['groupid'],group['name'])
-
   d = dict()
   hosts = z.host.get(['hostid', 'proxy_hostid', 'host', 'status', 'ipmi_authtype', 'ipmi_privilege', 'ipmi_username', 'ipmi_password', 'maintenanceid', 'maintenance_status', 'maintenance_type', 'maintenance_from', 'name', 'flags', 'templateid', 'description', 'tls_connect', 'tls_accept', 'tls_issuer', 'tls_subject', 'proxy_address', 'auto_compress', 'custom_interfaces', 'uuid', 'inventory_mode', 'active_available'])#output=['hostid','name']
   for host in hosts:
@@ -22,21 +20,17 @@ for group in groups:
     if key not in d:
       d[key] = dict()
       d[key]["name"] = host['name']
-
   hosts = z.hostinterface.get(['interfaceid', 'hostid', 'main', 'type', 'useip', 'ip', 'dns', 'port', 'available', 'error', 'errors_from', 'disable_until', 'details'])#output=['ip','interfaceid','hostid']
   for host in hosts:
     key = host['hostid']
     if key in d:
       d[key]["ip"] = host['ip']
       d[key]["error"] = host['error']
-    #f_out.write(host['hostid'] +  '|' + host['name'] + '\n')
-    #print((host.keys()))
 
 with open(link + name, 'w') as f_out: 
     for key in d.keys():
       t = "{}|{}|{}|{}\n".format(str(key),d[key]["name"],d[key]["ip"],d[key]["error"])
       f_out.write(t)
-        #f_out.append(host['hostid'] + '|' + host['ip'] +  '|' + host['error'] + '\n')
-        #print((host.keys()))
 print('Save txt file')
+
 print('Finish')
